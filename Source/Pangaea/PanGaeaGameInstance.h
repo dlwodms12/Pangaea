b64223907e4912e6245a5603bb32c1765e36e931
Jae Eun <pdw05498@naver.com>
2026-03-27 11:39:34 +0900

    게임 플레이 프레임 워크 생성

---
+++ Source/Pangaea/PanGaeaGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PanGaeaGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PANGAEA_API UPanGaeaGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Pangaea/PangaeaGameStateBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "PangaeaGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PANGAEA_API APangaeaGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
