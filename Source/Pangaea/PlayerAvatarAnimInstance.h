f2ff0a3c18d6c9c15d83efebb72ca7544123fdf0
Jae Eun <pdw05498@naver.com>
2026-03-29 07:01:00 +0900

    캐릭터 애니메이션을 제어하기 위한 PlayerAvatarAnimInstance 클래스 추가

---
+++ Source/Pangaea/PlayerAvatarAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerAvatarAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PANGAEA_API UPlayerAvatarAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
