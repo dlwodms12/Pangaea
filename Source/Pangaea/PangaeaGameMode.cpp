afb3350b44006642d7e86d185fa53044003006c5
Jae Eun <pdw05498@naver.com>
2026-03-29 06:52:35 +0900

    오류 수정

--- Source/Pangaea/PangaeaGameMode.cpp
+++ Source/Pangaea/PangaeaGameMode.cpp
@@ -11,7 +11,7 @@ APangaeaGameMode::APangaeaGameMode()
 	PlayerControllerClass = APangaeaPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_PlayerAvatar"));
 	if (PlayerPawnBPClass.Class != nullptr)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
@@ -22,13 +22,5 @@ APangaeaGameMode::APangaeaGameMode()
 	if(PlayerControllerBPClass.Class != NULL)
 	{
 		PlayerControllerClass = PlayerControllerBPClass.Class;
-	}
-
-	//캐릭터 에셋을 찾고 그 결과를 DefaultPawnClass 시스템 변수에 할당하는 코드
-	static ConstructorHelpers::FClassFinder<APawn>
-	PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_PlayerAvatar"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 }
