a7399bb70ff6474ad49e6db32cfdef693cd764be
Jae Eun <pdw05498@naver.com>
2026-04-15 02:29:12 +0900

    코드 주석 추가
    무기 장착 및 해제 관련 코드 설명 추가

--- Source/Pangaea/Weapon.cpp
+++ Source/Pangaea/Weapon.cpp
@@ -20,6 +20,9 @@ void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 
+	//this는 무기 액터 본인을 의미함
+	//두번째 매개변수는 핸들러 이벤트 함수 주소를 의미함
+	//무기 자체가 충돌 이벤트를 받음
 	OnActorBeginOverlap.AddDynamic(this, &AWeapon::OnWeaponBeginOverlap);
 }
 
@@ -28,8 +31,9 @@ void AWeapon::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (Owner == nullptr)
+	if (Holder == nullptr)
 	{
+		//무기를 아무도 안들고 있으면 회전
 		FQuat rotQuat = FQuat(FRotator(0.0f, 300.0f * DeltaTime, 0.0f));
 		AddActorLocalRotation(rotQuat);
 	}
@@ -40,31 +44,41 @@ void AWeapon::OnWeaponBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
 {
 	UE_LOG(LogTemp, Log, TEXT("Weapon overlapped"));
 
+	//캐릭터인지 확인
 	auto character = Cast<ACharacter>(OtherActor);
-
 	if (character == nullptr)
 	{
 		return;
 	}
 
+	//아무도 안들고 있는 무기라면
 	if (Holder == nullptr)
 	{
+		//플레이어인지 확인하고
 		auto playerAvatar = Cast<APlayerAvatar>(character);
-
 		if (playerAvatar != nullptr)
 		{
+			//무기의 주인을 등록
 			Holder = character;
 
+			//캐릭터가 들고 있는 모든 액터를 가져와서
 			TArray<AActor*> attachedActors;
 			OtherActor->GetAttachedActors(attachedActors, true);
 			for (int i = 0; i < attachedActors.Num(); ++i)
 			{
+				//DetachFromActor 함수는 무기를 현재 위치에 떨어뜨리는 기능을 수행함
+				//기존 가지고 있는 무기를 떨어뜨림
 				attachedActors[i]->DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
 				attachedActors[i]->SetActorRotation(FQuat::Identity);
 				AWeapon* weapon = Cast<AWeapon>(attachedActors[i]);
+				//떨어뜨린 무기는 주인을 해제하고 바닥 아이템이 됨
 				weapon->Holder = nullptr;
 			}
 
+			//첫번째 매개변수는 플레이어 캐릭터의 스켈레탈 메시를 전달
+			//두번째 매개변수는 엔진이 정의한 enum값으로, 함수가 무기를 메시의 스켈레톤 타깃 노드에 장착하도록 함
+			//세번째 매개변수는 타깃 소켓의 이름
+			//등록한 무기를 장착함
 			AttachToComponent(Holder->GetMesh(),
 				FAttachmentTransformRules::SnapToTargetIncludingScale, FName("hand_rSocket"));
 		}
--- Source/Pangaea/Weapon.h
+++ Source/Pangaea/Weapon.h
@@ -17,9 +17,12 @@ public:
 	// Sets default values for this actor's properties
 	AWeapon();
 
+	//무기가 플레이어에 장착됐을 때나 적이 무기를 집어들었을 때 설정됨. 
+	//또한 무기를 획득하지 않은 상태에서 무기가 회전하고 있는지 여부를 판단할 때도 사용.
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	ACharacter* Holder = nullptr;
 
+	//타깃에 전달되는 대미지를 계산할 때 사용
 	UPROPERTY(EditAnywhere, Category = "Weapon Params")
 	float Strength = 10;
 
@@ -31,8 +34,11 @@ protected:
 	UStaticMeshComponent* _StaticMesh;
 
 	UFUNCTION()
+	//BeginOverlap 핸들러 함수
+	//기능이 동작하려면 Weapon.cpp 에서 BeginPlay() 함수 무기 액터의 델리게이트 이벤트와 이 함수를 바인딩해야함
 	void OnWeaponBeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
 
+	//추후 구현
 	bool IsWithinAttackRange(float AttackRange, AActor* Target);
 
 public:
